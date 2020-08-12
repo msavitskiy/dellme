5e9d3388c68e4523b32cce765218f1db3fbe364f
msavitskiy <msavitskiy@users.noreply.github.com>
2020-08-13 01:44:43 +0300

    Update Test2.java

--- test2/Test2.java
+++ test2/Test2.java
@@ -1,1 +1,15 @@
-Test2 {{test2}}
+package com.{{test2}}.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class {{test2}}Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run({{test2}}Application.class, args);
+    }
+
+}
